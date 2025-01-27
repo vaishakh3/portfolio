8ab1360d0538e0eb5781da7f33fe9c49874808b1
vaishakh3 <vichured@gmail.com>
2025-01-28 01:24:15 +0530

    update bio

--- src/data/user.js
+++ src/data/user.js
@@ -16,9 +16,9 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Frontend developer, UI/UX Designer, Growth Hacker and amateur chef.",
+		title: "Software developer, UI/UX Designer, Growth Hacker and amateur chef.",
 		description:
-			"I am a frontend developer with expertise in React.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a software developer with expertise in React.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	about: {
